44232b5e61b0cbe1019b2b00308b22a03b16db75
Matheus Martins <matheusmr00@gmail.com>
2020-05-02 18:11:41 -0300

    Updates

---
+++ versions/second-microfront/0.1.0/precache-manifest.c14d3b84e9769212bec7296802e8c11f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a239975069b7c777178fdb7820864a1f",
+    "url": "./microfrontends/second-microfront/index.html"
+  },
+  {
+    "revision": "7249e17367711eb30177",
+    "url": "./microfrontends/second-microfront/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "7554745e56a072df1c5d",
+    "url": "./microfrontends/second-microfront/static/js/2.e1a29eba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./microfrontends/second-microfront/static/js/2.e1a29eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7249e17367711eb30177",
+    "url": "./microfrontends/second-microfront/static/js/main.126d1825.chunk.js"
+  },
+  {
+    "revision": "6b41953d809b22f2750a",
+    "url": "./microfrontends/second-microfront/static/js/runtime-main.0f4272b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./microfrontends/second-microfront/static/media/logo.5d5d9eef.svg"
+  }
+]);
