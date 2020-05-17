ad581658ffcb42d2079a8d0f8aa07d2f2b003c0b
Matheus Martins <matheusmr00@gmail.com>
2020-05-17 18:19:39 -0300

    my commit

--- v/beta/service-worker.js
+++ v/beta/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5193715eab299acd739656a1a14035da.js"
+  "./precache-manifest.c762d8cf36b4361c99ddf6a7745db209.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
