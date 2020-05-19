e606239b25f543850e1d9a7dc238b9c91a44ab9a
Matheus Martins <matheusmr00@gmail.com>
2020-05-18 22:20:22 -0300

    my commit

--- v/beta/precache-manifest.2f5e54512ae0ec39c6ab41b0189edc16.js
+++ v/beta/precache-manifest.2f5e54512ae0ec39c6ab41b0189edc16.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "401b080b37945110a10b5557f70b4f3f",
+    "revision": "4fdec2b808caeffd261b3ed591389242",
     "url": "./index.html"
   },
   {
-    "revision": "18c4d0255425fd20082d",
-    "url": "./static/css/main.32cfb512.chunk.css"
+    "revision": "2d4c4877cc38c1aba9d2",
+    "url": "./static/css/main.e002a324.chunk.css"
   },
   {
     "revision": "18dd146ef79c8b9622bb",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.30e9f370.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "18c4d0255425fd20082d",
+    "revision": "2d4c4877cc38c1aba9d2",
     "url": "./static/js/main.45e7a82a.chunk.js"
   },
   {
